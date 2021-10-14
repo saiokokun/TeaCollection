53c36432100194e6ba9f1a8b63bacb733ddbd47d
Your Namesaiokokun <grigory.backman@student.kyh.se>
2021-10-14 15:23:43 +0200

    u

---
+++ src/WhatTea/TeaDatabase.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class TeaDatabase {
+    public TeaDatabase(String[] args) {
+        ArrayList Tesort = new ArrayList();
+        Tesort.add("Grön te");
+        Tesort.add("Svart Te");
+        Tesort.add("Herb te");
+        Tesort.add("Vit te");
+        System.out.println(Tesort);
+    }
+}
---
+++ src/WhatTea/main.java
@@ -0,0 +1,11 @@
+package WhatTea;
+public class main {
+        public static void main(String[] args) {
+            WhatTea tag = new WhatTea();
+
+            tag.TeaDatabase();
+            tag.WhatTea();
+            //tag.quit();
+
+        }
+}
