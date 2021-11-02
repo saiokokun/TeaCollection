167793606f86830adee24d38354afa62ba44fe9b
Your Namesaiokokun <grigory.backman@student.kyh.se>
2021-11-02 14:58:19 +0100

    Nu kan man ta bort te

--- src/WhatTea/TeMeny.java
+++ src/WhatTea/TeMeny.java
@@ -1,7 +1,4 @@
 package WhatTea;
-
-
-import javax.naming.Name;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -17,7 +14,6 @@ public class TeMeny {
             // 2. Läs in kommando från användaren
             String input = userInput.nextLine();
              if (input.equalsIgnoreCase("1")) {
-
                 System.out.println("Du valde 1");
                  System.out.println("Vad ska teet heta?");
                  String newTeaName = userInput.nextLine();
@@ -30,11 +26,25 @@ public class TeMeny {
                  ArrayList<Tea> TeaSaver = Save.GetAllTeas();
                  TeaSaver.add(New);
                  SparaTe.StoreAllTeas(TeaSaver);
-
-
                 }
             else if (input.equalsIgnoreCase("2")) { //Ta bort te
                 System.out.println("Du valde 2");
+                 ArrayList<Tea> TeaLoader = SparaTe.GetAllTeas();
+                 System.out.println("Är du säker på att ta bort te? Y/N");
+                 String userInputSure = userInput.nextLine();
+                 if (userInputSure.equalsIgnoreCase("Y")) {
+                     for (int i = 0; i < TeaLoader.size(); i++) {
+                         System.out.println("\n Number " + (i) + "\n|Name:| " + TeaLoader.get(i).Name + " " + "\n|Description:| " + TeaLoader.get(i).Description + " " + "\n|Cooktime:| " + TeaLoader.get(i).CookTime + "\n");
+                     }
+                     System.out.println("Vilken till du ta bort? Skriv nummer");
+                     System.out.print("\n> ");
+                     int taBortInput = Integer.parseInt(userInput.nextLine());
+                     TeaLoader.remove(taBortInput);
+                     SparaTe.StoreAllTeas(TeaLoader);
+                 }
+                 else {
+                     System.out.println("Ångrat, återvänder\n");
+                 }
             }
              else if (input.equalsIgnoreCase("3")) { //Random te
                  System.out.println("Du valde 3");
@@ -44,9 +54,8 @@ public class TeMeny {
                  Random TeaRandom = new Random();
                  int upperbound = NumberSize;
                  int int_random = TeaRandom.nextInt(upperbound);
-                 System.out.println("\n|Name:| " + TeaLoader.get(int_random).Name + " " + "\n|Description:| " + TeaLoader.get(int_random).Description + " " + "\n|Cooktime:| " + TeaLoader.get(int_random).CookTime);
+                 System.out.println("\n|Name:| " + TeaLoader.get(int_random).Name + " " + "\n|Description:| " + TeaLoader.get(int_random).Description + " " + "\n|Cooktime:| " + TeaLoader.get(int_random).CookTime + "\n");
              }
-
              else if (input.equalsIgnoreCase("4")) { //Visa alla teer
                  System.out.println("Du valde 4");
                  //System.out.println(SparaTe.GetAllTeas());
@@ -56,18 +65,11 @@ public class TeMeny {
                  for (int i = 0; i < TeaLoader.size(); i++) {
                      System.out.println("\n|Name:| " + TeaLoader.get(i).Name + " " + "\n|Description:| " + TeaLoader.get(i).Description + " " + "\n|Cooktime:| " + TeaLoader.get(i).CookTime + "\n");
                  }
-
              }
              else if(input.equalsIgnoreCase("quit")) {
                  return input;
-                 //running = false;
              }
         }
-
         return null;
     }
-
-
-
-
 }
--- src/WhatTea/main.java
+++ src/WhatTea/main.java
@@ -1,24 +1,10 @@
 package WhatTea;
-
-import java.io.IOException;
-import java.util.ArrayList;
 //ladda alla classer
 public class main {
         public static void main(String[] args) {
-
             TeaDatabase tag = new TeaDatabase();
             TeMeny load = new TeMeny();
             load.TeaInteraction(tag);
-            //tag.toString();
             tag.StorageExists();
-            //Om filen är tom
-            //ArrayList<Tea> TeaSaver = new ArrayList<Tea>();
-            /*Tea GronTe = new Tea("Grön te", "1 min");
-            Tea SvartTe = new Tea("Svart", "2min");
-            TeaSaver.add(GronTe);
-            TeaSaver.add(SvartTe);*/
-            //tag.StoreAllTeas(TeaSaver);
-            ArrayList<Tea> GetAllTeas = tag.GetAllTeas();
-
         }
 }
